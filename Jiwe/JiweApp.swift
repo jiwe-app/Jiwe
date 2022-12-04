2e08b07a6372c4eaa7cbc18ef8cb17fdd2d95bc6
Zaitam <gotzmatiascode@gmail.com>
2022-12-03 21:49:29 -0300

    Initial Commit

---
+++ Jiwe/JiweApp.swift
@@ -0,0 +1,17 @@
+//
+//  JiweApp.swift
+//  Jiwe
+//
+//  Created by Zaitam on 03/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct JiweApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
